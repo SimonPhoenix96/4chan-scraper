0c1e2ad3a07e7e22e153af5bd3294ebc5f64766c
simonphoenix96 <diazjustin1995@yahoo.de>
2021-03-20 22:41:40 +0100

    add handling user_id trips + add exception handling

--- 4chan-scraper.py
+++ 4chan-scraper.py
@@ -11,82 +11,84 @@ import argparse
 def get_board_threads(boards):
 
     for board in boards:
-        url = "http://4chan.org/" + board + "/catalog"
-        html = requests.get(url).text 
-        data = {}
-        data['threads'] = []
+        try: 
+            url = "http://4chan.org/" + board + "/catalog"
+            html = requests.get(url).text 
+            data = {}
+            data['threads'] = []
 
-        thread_start = [x.start() for x in finditer('":{"date"', html)]
+            thread_start = [x.start() for x in finditer('":{"date"', html)]
 
-        for indice in thread_start:
-            thread_id = ""
-            counter = 1
-            while html[indice-counter] != '"':
-                thread_id = html[indice-counter] + thread_id
-                counter += 1
-            data['threads'].append({'thread_board': board , 'thread_id': thread_id,  'thread_url': 'https://boards.4chan.org/' + board + '/thread/' + thread_id})
-
-        for item in data['threads']:
-            indicies = [x.start() for x in finditer(item['thread_id'], html)]
-            # print(indicies)
-            for indice in indicies:
-                thread_crawler = ""
+            for indice in thread_start:
+                thread_id = ""
                 counter = 1
-                while False != True:
-                    # 
-                    # 
-                    thread_crawler = thread_crawler + html[(indice-2)+counter] 
-                                
+                while html[indice-counter] != '"':
+                    thread_id = html[indice-counter] + thread_id
                     counter += 1
-                    # print(thread_crawler)
-                    subject_start = thread_crawler.find(',"sub":"')
-                    if subject_start != -1: 
-                        # add index where sub html attribute starts to index where specific thread id starts
-                        subject_start = subject_start + indice
-                        break
-                # print("subject start index: " + str(subject_start) + " subject content index: " +  str(html[subject_start]) )
-                
-                thread_crawler = ""
-                counter = 1
-                while False != True:
-                    # 
-                    # 
-                    thread_crawler = thread_crawler + html[(subject_start-2)+counter] 
-                    # print(html[indice+counter])
-                    counter += 1
-                    # print(thread_crawler)
-                    teaser_start = thread_crawler.find(',"teaser":"')
-                    if teaser_start != -1: 
-                        # add index where teaser html attribute starts to index where specific thread id starts
-                        teaser_start = teaser_start + subject_start
-                        break
-                subject_end = teaser_start-1        
-                # print("subject content: " + str(html[subject_start+6:subject_end]) )
-                # print("teaser start index: " + str(teaser_start) + " teaser content index: " +  str(html[teaser_start+9]) )
-                item.update({'thread_subject': html[subject_start+7:subject_end-1]})
-                thread_crawler = ""
-                counter = 1
-                while False != True:
+                data['threads'].append({'thread_board': board , 'thread_id': thread_id,  'thread_url': 'https://boards.4chan.org/' + board + '/thread/' + thread_id})
+
+            for item in data['threads']:
+                indicies = [x.start() for x in finditer(item['thread_id'], html)]
+                # print(indicies)
+                for indice in indicies:
+                    thread_crawler = ""
+                    counter = 1
+                    while False != True:
+                        # 
+                        # 
+                        thread_crawler = thread_crawler + html[(indice-2)+counter] 
+                                    
+                        counter += 1
+                        # print(thread_crawler)
+                        subject_start = thread_crawler.find(',"sub":"')
+                        if subject_start != -1: 
+                            # add index where sub html attribute starts to index where specific thread id starts
+                            subject_start = subject_start + indice
+                            break
+                    # print("subject start index: " + str(subject_start) + " subject content index: " +  str(html[subject_start]) )
                     
-                    thread_crawler = thread_crawler + html[(subject_end-2)+counter] 
-                    # print(html[indice+counter])
-                    counter += 1
-                    # print(thread_crawler)
+                    thread_crawler = ""
+                    counter = 1
+                    while False != True:
+                        # 
+                        # 
+                        thread_crawler = thread_crawler + html[(subject_start-2)+counter] 
+                        # print(html[indice+counter])
+                        counter += 1
+                        # print(thread_crawler)
+                        teaser_start = thread_crawler.find(',"teaser":"')
+                        if teaser_start != -1: 
+                            # add index where teaser html attribute starts to index where specific thread id starts
+                            teaser_start = teaser_start + subject_start
+                            break
+                    subject_end = teaser_start-1        
+                    # print("subject content: " + str(html[subject_start+6:subject_end]) )
+                    # print("teaser start index: " + str(teaser_start) + " teaser content index: " +  str(html[teaser_start+9]) )
+                    item.update({'thread_subject': html[subject_start+7:subject_end-1]})
+                    thread_crawler = ""
+                    counter = 1
+                    while False != True:
+                        
+                        thread_crawler = thread_crawler + html[(subject_end-2)+counter] 
+                        # print(html[indice+counter])
+                        counter += 1
+                        # print(thread_crawler)
 
-                    # end of threads is noted by end of html thread array which looks like this in the html "}},
-                    teaser_end = thread_crawler.find('"},') + thread_crawler.find('"}},')
-                    if teaser_end != -2: 
-                        # add index where teaser html attribute starts to index where specific thread id starts
-                        teaser_end = teaser_end + subject_end
-                        break
-                # print("teaser content: " + str(html[teaser_start+9:teaser_end]) )
-                item.update({'thread_teaser': html[teaser_start+10:teaser_end]})
+                        # end of threads is noted by end of html thread array which looks like this in the html "}},
+                        teaser_end = thread_crawler.find('"},') + thread_crawler.find('"}},')
+                        if teaser_end != -2: 
+                            # add index where teaser html attribute starts to index where specific thread id starts
+                            teaser_end = teaser_end + subject_end
+                            break
+                    # print("teaser content: " + str(html[teaser_start+9:teaser_end]) )
+                    item.update({'thread_teaser': html[teaser_start+10:teaser_end]})
 
-            # print(item)
-        
-        
-        # Omit the first post, because it's always a
-        # mod's sticky for the board
+                # print(item)
+        except:
+            print("An exception occurred with: " + str(boards))
+            
+    # Omit the first post, because it's always a
+    # mod's sticky for the board
     return data['threads'][1:] 
 
 def get_thread_posts(thread_html):
@@ -96,53 +98,67 @@ def get_thread_posts(thread_html):
     op_post_parsed = False
     
     for post in thread_html:
-        # try:
-        # # fileText html parsing 
-        file_html =  post.find(attrs={'class': 'fileText'})
-        file_url = ""
-        file_name = ""
+        try:
+            # # fileText html parsing 
+            file_html =  post.find(attrs={'class': 'fileText'})
+            file_url = ""
+            file_name = ""
 
-        if file_html != None:
-            file_url = sub("//", "https://", file_html.find("a").get('href'))
-            file_name = file_html.find("a").contents[0]
-        
-        
-        # # postInfo html parsing
-        if not op_post_parsed or post.find(attrs={'class': 'post op'}) != None:
-            # # op html parsing
-            post_html = post.find(attrs={'class': 'post op'})
-            op_post_parsed = True
-            post_type = "op"
-
-        else: 
-            # # reply html parsing
-            post_html = post.find(attrs={'class': 'post reply'})
-            post_type = "reply"
-
-        post_user = ""
-        post_user_id = ""
-        post_date = ""
-        post_number = ""
-        post_message = ""
-    
-        # # get postinfo
-        if post_html != None:
-
-            # Extract Post Info
-            post_info =  post_html.find(attrs={'class': 'postInfo desktop'})
-            post_user =  post_info.find(attrs={'class': 'name'}).contents[0]
-            post_user_id = "none" or post_info.find(attrs={'class': 'hand'}).contents[0]
-            post_date =  post_info.find(attrs={'class': 'dateTime'}).contents[0]
-            post_number =  post_info.find('input').get('name')
+            if file_html != None:
+                file_url = sub("//", "https://", file_html.find("a").get('href'))
+                file_name = file_html.find("a").contents[0]
             
-            # Extract Post Message
-            temp_post_message = post_html.find(attrs={'class': 'postMessage'})
-            len_post_message = len(temp_post_message)
-            if len_post_message == 0:
-                # no message
-                post_message = " "
-            else:
-                # message
+            
+            # # postInfo html parsing
+            if not op_post_parsed or post.find(attrs={'class': 'post op'}) != None:
+                # # op html parsing
+                post_html = post.find(attrs={'class': 'post op'})
+                op_post_parsed = True
+                post_type = "op"
+
+            else: 
+                # # reply html parsing
+                post_html = post.find(attrs={'class': 'post reply'})
+                post_type = "reply"
+
+            post_user = " "
+            post_user_id = " "
+            post_date = " "
+            post_number = " "
+            post_message = " "
+        
+            # # get postinfo
+            if post_html != None:
+
+
+                # Extract Post Info
+                post_info =  post_html.find(attrs={'class': 'postInfo desktop'})
+                # print(post_info.find(attrs={'class': 'name'}))
+                post_number =  post_info.find('input').get('name') or "none"
+                post_date =  post_info.find(attrs={'class': 'dateTime'}).contents[0] or "none"
+                
+                # this handles users with trips
+                temp_post_info = post_info.find(attrs={'class': 'name'}).contents
+                if len(temp_post_info) != 0:
+                    post_user = post_info.find(attrs={'class': 'name'}).contents[0]
+                else:
+                    post_user = post_info.find(attrs={'class': 'postertrip'}).contents[0]
+                
+                # some boards dont have user ids, this handles em
+                temp_post_user_id = post_info.find(attrs={'class': 'hand'})
+                if temp_post_user_id == None:
+                    post_user_id =  "none"
+                else:
+                    post_user_id =  post_info.find(attrs={'class': 'hand'}).contents[0]
+                
+                # Extract Post Message
+                temp_post_message = post_html.find(attrs={'class': 'postMessage'}) or "none"
+                len_post_message = len(temp_post_message)
+                # if len_post_message == 0:
+                #     # no message
+                #     post_message = " "
+                # else:
+                    # message
                 for line in temp_post_message.contents:
                     # print(line)
                     quotelink = re.search('">(.*)</a>', str(line))
@@ -153,35 +169,35 @@ def get_thread_posts(thread_html):
                     
                     post_message = post_message + ''.join(str(line)).replace("\u2019", "'").replace('<span class="quote">&gt;', ">").replace('<br/>', "\n").replace('</span>', "")
 
-        posts['reply'].append({ "post_type" : post_type,
-                                "post_user": post_user,
-                                "post_user_id": post_user_id,
-                                "post_date": post_date,
-                                "post_number": post_number,
-                                "post_message": post_message,
-                                "file_url" : file_url,
-                                "file_name": file_name,})
-    # except:
-    #     print("An exception occurred with: " + str(post))
+            posts['reply'].append({ "post_type" : post_type,
+                                    "post_user": post_user,
+                                    "post_user_id": post_user_id,
+                                    "post_date": post_date,
+                                    "post_number": post_number,
+                                    "post_message": post_message,
+                                    "file_url" : file_url,
+                                    "file_name": file_name,})
+        except:
+            print("An exception occurred with: " + str(post))
 
     return posts['reply']
 
 def get_thread(threads):
     
     for thread in threads:
-        # try:
-        # parse url 
-        parsed_url = urlparse(thread['url'])
-        #local function variables
-        thread_id = parsed_url.path.split('/')[-1]
-        thread_board = parsed_url.path.split('/')[1]
-        # web request
-        html = requests.get(thread['url']).text
-        soup = BeautifulSoup(html, 'html.parser') 
-        thread_html = soup.find_all(attrs={'class': 'thread'})[0] 
-        thread.update({"thread_id" : thread_id, "thread_board" : thread_board, "thread_posts" : get_thread_posts(thread_html)})
-        # except:
-        #     print("An exception occurred with: " + str(thread['url']))
+        try:
+            # parse url 
+            parsed_url = urlparse(thread['url'])
+            #local function variables
+            thread_id = parsed_url.path.split('/')[-1]
+            thread_board = parsed_url.path.split('/')[1]
+            # web request
+            html = requests.get(thread['url']).text
+            soup = BeautifulSoup(html, 'html.parser') 
+            thread_html = soup.find_all(attrs={'class': 'thread'})[0] 
+            thread.update({"thread_id" : thread_id, "thread_board" : thread_board, "thread_posts" : get_thread_posts(thread_html)})
+        except:
+            print('{"Exception" : "An exception occurred with: ' + str(thread['url']) + '"}' )
     return thread
 
 def main():
